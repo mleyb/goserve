50037d306edb795390d90de228901491cd39dca3
Mark Leybourne <mleyb@users.noreply.github.com>
2018-04-22 13:13:15 +0100

    Emit hostname in response

--- main.go
+++ main.go
@@ -2,20 +2,23 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
-
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", index)
 	log.Fatal(http.ListenAndServe(":8081", router))
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	hostname, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Fprintf(w, "Hello from a Go service on host %q", hostname)
 }
